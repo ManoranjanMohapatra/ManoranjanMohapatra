2c54549c927a2c8a70e727bb62c0f538bdd8793e
ManoranjanMohapatra <manoranjanmohapatra2002@gmail.com>
2024-08-15 13:30:16 +0530

    first commit

---
+++ app1/views.py
@@ -0,0 +1,5 @@
+from django.http import HttpResponse
+def longBoat(request):
+    return HttpResponse('This is a good pub for drinking')
+def radhaKrushna(request):
+    return HttpResponse('Bas apki bat man jao mein')
