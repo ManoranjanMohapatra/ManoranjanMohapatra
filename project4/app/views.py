f37c7fa506bfd5474e06ca2ad374c11b6cb5e538
ManoranjanMohapatra <manoranjanmohapatra2002@gmail.com>
2024-08-18 11:49:16 +0530

    Add files via upload

---
+++ project4/app/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+def pikun(request):
+    return render(request,'pikun.html')
+def radha(request):
+    return render(request,'second.html')
